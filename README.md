fcabee2f6f0b0b0f78084c8ab99b6da8a1dedbf7
James Newell <james.newell@lendi.com.au>
2019-02-18 22:33:20 +1100

    fix koa-create-context types and fix build scripts

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A number of packages made to work with koa.
 
 ## [koa-create-context](./koa-create-context)
 
+Create a context for mocking or whatever purpose.
+
 ## [koa-router-middleware](./koa-router-middleware)
 
-A router for koa.
+A router middleware for koa.
--- koa-create-context/src/index.ts
+++ koa-create-context/src/index.ts
@@ -2,17 +2,20 @@ import * as Koa from "koa";
 import * as MockReq from "mock-req";
 import * as MockRes from "mock-res";
 
-export interface Options {
+export type Options<StateT, CustomT> = {
   req?: any;
   res?: any;
+  state?: StateT;
   [name: string]: any;
-}
+} & Partial<CustomT>;
 
-export default function<StateT = any, CustomT = {}>(options: Options = {}) {
+export default function<StateT = any, CustomT = {}>(
+  options: Options<StateT, CustomT> = {}
+): Koa.ParameterizedContext<StateT, CustomT> {
   const app = new Koa<StateT, CustomT>();
   const req = new MockReq(options.res || {});
   const res = new MockRes(options.req || {});
-  const context = app.createContext(req, res);
+  const context = app.createContext<StateT>(req, res);
 
   Object.keys(options).forEach(key => {
     if (key !== "req" && key !== "res") {
@@ -20,5 +23,5 @@ export default function<StateT = any, CustomT = {}>(options: Options = {}) {
     }
   });
 
-  return context;
+  return context as Koa.ParameterizedContext<StateT, CustomT>;
 }
